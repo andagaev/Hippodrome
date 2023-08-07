87e444aba8d1d00aa265189cceec54f2d8d8801f
Andrei Agaev <andagaev@gmail.com>
2023-08-06 21:32:00 -0700

    add horse constructor

--- src/Horse.java
+++ src/Horse.java
@@ -2,4 +2,10 @@ public class Horse {
     public String name;
     public double speed;
     public double distance;
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
 }
