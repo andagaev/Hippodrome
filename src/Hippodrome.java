72ec9fe6d7afa0538ea84f4f3306c7b0eae79217
Andrei Agaev <andagaev@gmail.com>
2023-08-08 11:03:37 -0700

    add print, move, run methods

--- src/Hippodrome.java
+++ src/Hippodrome.java
@@ -10,7 +10,35 @@ public class Hippodrome {
         return horses;
     }
 
-    public static void main(String[] args) {
+    public static Hippodrome game;
 
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+
+    public void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            Thread.sleep(300);
+        }
+    }
+
+    public void move() {
+        for (int i = 0; i < horses.size(); i++) {
+            horses.get(i).move();
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < horses.size(); i++) {
+            horses.get(i).print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
     }
 }
