c163d04a8be32dd4ca5b5f6911b9a8367a3b34bb
Andrei Agaev <andagaev@gmail.com>
2023-08-08 10:32:02 -0700

    add getters and setters, method move, print

--- src/Horse.java
+++ src/Horse.java
@@ -8,5 +8,40 @@ public class Horse {
         this.speed = speed;
         this.distance = distance;
     }
-    
+
+    public void move() {
+        distance += speed * Math.random();
+    }
+
+    public void print() {
+        for (int i = 1; i < distance; i++) {
+            System.out.print(".");
+        }
+        System.out.print(name);
+        System.out.println();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
 }
